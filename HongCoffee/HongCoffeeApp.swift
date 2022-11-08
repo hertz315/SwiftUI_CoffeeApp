29270e878695238d0b126afd7c06b3ae7e5663a3
hertz315 <hertz315@gmail.com>
2022-11-08 09:35:04 +0900

    Initial Commit

---
+++ HongCoffee/HongCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongCoffeeApp.swift
+//  HongCoffee
+//
+//  Created by Hertz on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HongCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
